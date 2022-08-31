8c1c6a34c6acf73a2664094405bb05a9e512d3c8
Mozib <mozib.diu@gmail.com>
2022-08-31 16:55:43 +0600

    Need to increase smoothness

---
+++ index.js
@@ -0,0 +1,25 @@
+let scene = document.getElementById('scene');
+let cube = document.getElementById('cube');
+
+let originalVideo = document.getElementById('myVideo');
+let videoHtml = `<video src="resources/gp.mp4" muted="false" autoplay="autoplay" controls controlslist="nofullscreen " id="gp-video"
+class="gp-video" onended="endOpearation()"></video>`
+
+function firstOp() {
+    cube.classList.add('cubepic-animation');
+    cube.classList.remove('hidden');
+
+}
+
+firstOp();
+cube.addEventListener('animationend', () => {
+    
+    cube.classList.remove('cubepic-animation');
+    cube.classList.add('hidden');
+    originalVideo.innerHTML = videoHtml;
+});
+
+function endOpearation() {
+    originalVideo.innerHTML = '';
+    firstOp();
+}
