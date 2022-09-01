6f80a207b40e5abf3464079ae2c456a2dd12f7ff
Mozib <mozib.diu@gmail.com>
2022-09-01 11:00:04 +0600

    video transition working

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 let scene = document.getElementById('scene');
 let cube = document.getElementById('cube');
+let backSide = document.getElementById('backSide');
 
 let originalVideo = document.getElementById('myVideo');
 let videoHtml = `<video src="resources/gp.mp4" muted="false" autoplay="autoplay" controls controlslist="nofullscreen " id="gp-video"
 class="gp-video" onended="endOpearation()"></video>`
 
 function firstOp() {
-    cube.classList.add('cubepic-animation');
-    cube.classList.remove('hidden');
+    cube.classList.remove('cubepic-animation');
+    
+
+    setTimeout(() => {
+        cube.classList.add('cubepic-animation');
+    }, 100);
+    
+    setTimeout(()=>{
+        backSide.innerHTML = videoHtml;
+        setTimeout(() => {
+            cube.style.animationPlayState = 'paused';
+        }, 2500);
+    },3000);
 
 }
 
 firstOp();
 cube.addEventListener('animationend', () => {
-    cube.classList.add('hidden');
-    cube.classList.remove('cubepic-animation');
-    originalVideo.innerHTML = videoHtml;
+    firstOp();
 });
 
 function endOpearation() {
-    originalVideo.innerHTML = '';
-    firstOp();
+    cube.style.animationPlayState = 'running';
+    setTimeout(() => {
+        backSide.innerHTML = '';
+    }, 3000);
 }
